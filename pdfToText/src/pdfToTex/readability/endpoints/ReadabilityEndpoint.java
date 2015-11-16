5ade6bd792a6a2fd4561bd72c8f29516f0f2dde2
bestbeer <bestbeer@gmail.com>
2015-11-16 12:21:47 +0200

    partially writeToDB func - still need to finish

--- pdfToText/src/pdfToTex/readability/endpoints/ReadabilityEndpoint.java
+++ pdfToText/src/pdfToTex/readability/endpoints/ReadabilityEndpoint.java
@@ -5,6 +5,8 @@ import pdfToText.dao.*;
 import pdfToText.readability.engine.Readability;
 import pdfToText.readability.enums.MetricType;
 import pdfToText.vo.Paper;
+import pdfToText.vo.PaperLingQual;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,6 +36,22 @@ public class ReadabilityEndpoint {
     public void writeToDbReadability (Map<MetricType, BigDecimal> readabilityMetrics, String doi)
     {
     	//will read all the txt files in the dir and write to the DB Table the readability results per DOI
+    	PaperLingQual paperLingQual = new PaperLingQual();
+    	paperLingQual.setDoi(doi);
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setCHARACTERS(readabilityMetrics.get(MetricType.CHARACTERS).intValue());
+    	paperLingQual.setCOLEMAN_LIAU(readabilityMetrics.get(MetricType.COLEMAN_LIAU).doubleValue());
+    	paperLingQual.setCOMPLEXWORDS(readabilityMetrics.get(MetricType.COMPLEXWORDS).intValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	paperLingQual.setARI(readabilityMetrics.get(MetricType.ARI).doubleValue());
+    	
+    	insertLingQual(PaperLingQual paperLingQual)
     	
     }
     
